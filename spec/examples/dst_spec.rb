f08cc41bfa2a5646a75e06a7c6f59c84935c62aa
Forrest Zeisler <forrest@getjobber.com>
2013-08-15 15:45:22 -0600

    fixing spec comments

--- spec/examples/dst_spec.rb
+++ spec/examples/dst_spec.rb
@@ -31,13 +31,13 @@ describe IceCube::Schedule, 'occurs_on?' do
   end
 
   # DST in 2013 is November 6th at 1:30
-  it 'crosses a daylight savings time boundary (in the other direction) with a recurrence rule in local time, by utc conversion' do
+  it 'not changing days with daily rule' do
     Time.zone = "America/Denver"
     start_at = Time.parse("Sun, 03 Nov 2013 01:30:00 MDT -06:00")
     schedule = IceCube::Schedule.new(start_at)
     schedule.add_recurrence_rule IceCube::Rule.daily
     next_occurance = schedule.next_occurrence(start_at)
-    next_occurance.to_date.should_not == start_at.to_date # FAIL
+    next_occurance.to_date.should_not == start_at.to_date
   end
 
   it 'cross a daylight savings time boundary with a recurrence rule in local time' do
